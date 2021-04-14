0e87e5635a2d635ac4afbeb8ce2530add6de7d14
Anjaliraha <54320622+Anjaliraha@users.noreply.github.com>
2021-04-14 14:50:28 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/d9te6tbalsrqapb/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/d9te6tbalsrqapb/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['NORMAL','PNEUMONIA']
